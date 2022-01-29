953ed4b0c836b299ffdbc0423a1ebc83cc03bde2
Matthew C Wake <mcwake-dev@outlook.com>
2022-01-29 19:37:31 +0000

    Increased timeout

--- __tests__/controllers/ticket.controller.test.js
+++ __tests__/controllers/ticket.controller.test.js
@@ -13,7 +13,7 @@ chai.use(require("chai-as-promised"));
 chai.use(require("chaid"));
 
 suite("Ticket Controller", function () {
-  this.timeout(20000);
+  this.timeout(30000);
 
   before(async () => {
     await setup();
